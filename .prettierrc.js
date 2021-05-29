3e5370bb6526cf97e377753ef047d0f5ba083ccd
Joe Pea <joe@trusktr.io>
2021-05-29 16:26:38 -0700

    add prettier

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    useTabs: false,
+    tabWidth: 4,
+    semi: true,
+    singleQuote: true,
+    trailingComma: 'none',
+    bracketSpacing: false,
+    printWidth: 120,
+    arrowParens: 'avoid',
+
+    overrides: [{files: '*.md', options: {tabWidth: 2}}]
+};
